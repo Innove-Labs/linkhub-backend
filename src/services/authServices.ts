3ea5547b4fddc657554c01f0466d7ac204dc92eb
fawazsullia <fawazsullia@gmail.com>
2022-09-16 18:01:02 +0400

    auth middleware, login handler, user service

--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -5,7 +5,7 @@ const saltRounds = 10
 
 const authServices : any = {}
 
-authServices.registerNewUser = async (email : string, password : string, userName : string) => {
+authServices.registerNewUserWithEmail = async (email : string, password : string, userName : string) => {
     const passwordHash = await bcrypt.hash(password, saltRounds);
     const user = new UserModel({
         email,
@@ -24,6 +24,12 @@ authServices.registerNewUser = async (email : string, password : string, userNam
 authServices.checkExistingUser = async (email : string) : Promise<boolean> => {
     const userExists = await UserModel.findOne({email : email}, "email")
     if(userExists) return true
+    return false
+}
+
+authServices.loginWithEmail = async (user : any, password : string) : Promise<boolean> => {
+    const passwordMatches = await bcrypt.compare(user.passwordHash, password)
+    if(passwordMatches) return true
     return false
 }
 
